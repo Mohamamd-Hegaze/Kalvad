d48dae53b1db16eb023504f26069450b2f968454
Mohamamd-Hegaze <MohammadFITE@gmail.com>
2023-05-11 20:56:12 +0400

    add validation attributes for valid email and phone number

--- Kalvad_API_Test/Kalvad_API_Test/Dto/CustomerDto.cs
+++ Kalvad_API_Test/Kalvad_API_Test/Dto/CustomerDto.cs
@@ -1,12 +1,24 @@
 ﻿using Kalvad_API_Test.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Kalvad_API_Test.Dto
 {
     public class CustomerDto
     {
+        [Required(ErrorMessage = "FirstName Required")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName Required")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "PhoneNumber Required")]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "EmailAddress Required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
 
         public Customer GetCustomer()
